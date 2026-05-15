d7ce45b10ecdbd1c0681748b958fdc8cc53cdb45
AngelRocca <angelrocca1@gmail.com>
2026-05-15 12:32:13 -0500

    fix: attemp to fix Vercel deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './app.vue'
+import App from './App.vue'
 import router from './router'
 import pinia from './pinia'
 import i18n from './i18n'
